8aff5db2830ca774c7e79d6de2b911a5a26c8c03
Roger6763 <aedanb000@gmail.com>
2024-11-10 22:29:37 +0000

    08

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -16,5 +16,15 @@ screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
 t = turtle.Turtle()
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+def set_turtle_image(turtle, image_name):
+   
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
 
-... # Your Code Here
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+   
+set_turtle_image(turtle, "pikachu.gif")
